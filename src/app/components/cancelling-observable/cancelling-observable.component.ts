19f0f615433122715b5c5ffea90b184272ec8048
Natalya <natasha-vd@yandex.ru>
2023-12-02 13:25:27 +0300

    create user Observable

--- src/app/components/cancelling-observable/cancelling-observable.component.ts
+++ src/app/components/cancelling-observable/cancelling-observable.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Subscription, interval } from 'rxjs';
+import { Observable, Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'app-cancelling-observable',
@@ -7,16 +7,44 @@ import { Subscription, interval } from 'rxjs';
   styleUrls: ['./cancelling-observable.component.css']
 })
 export class CancellingObservableComponent implements OnInit {
-  timerSubs!: Subscription
+  timerConsoleSubs!: Subscription
+  timerBrowserSubs!: Subscription
+  timers: number[] = []
 
   ngOnInit(): void {
-    const newObservable = interval(1000)
+    const newObservable = new Observable<number>((subscribe) => {
+      let i = 0
+      const interval = setInterval(() => {
+        console.log('Interval executing')
+        subscribe.next(i++)
 
-    this.timerSubs = newObservable.subscribe((data) => console.log(new Date().toLocaleTimeString() + ' ' + data))
+        if(i === 6) {
+          subscribe.error('unknown error occurred')
+        }
+      }, 1000)
+
+      // Для очистки чего-то (например, таймера) при ошибке или отмене подписки
+      return () => {
+        console.log('called when observer is unsubscribed')
+        clearInterval(interval)
+      }
+    })
+
+    this.timerConsoleSubs = newObservable.subscribe((data) => console.log(new Date().toLocaleTimeString() + ' ' + data))
+
+    this.timerBrowserSubs = newObservable.subscribe((data) => {
+      this.timers.push(data)
+    })
+
+    // Добавить/удалить одну подписку в другую
+    // Тогда при timerConsoleSubs отмене будет отменено и timerBrowserSubs (но timerBrowserSubs может недополучить последние данные)
+    // this.timerConsoleSubs.add(this.timerBrowserSubs)
+    // this.timerConsoleSubs.remove(this.timerBrowserSubs)
   }
 
   cancelTimer() {
     console.log('cancel the observable')
-    this.timerSubs.unsubscribe()
+    this.timerConsoleSubs.unsubscribe()
+    this.timerBrowserSubs.unsubscribe()
   }
 }
